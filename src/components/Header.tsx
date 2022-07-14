2880e7e30d287faa52e6f2e4cc8674d25d2c34f5
ThiagoSilvaMorato <thiago.morato@dartdigital.com.br>
2022-07-14 11:57:56 -0300

    Finalizando o Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Input, Text, Icon } from "@chakra-ui/react";
-import { RiSearchLine } from "react-icons/ri";
+import { Flex, Input, Text, Icon, HStack, Box, Avatar } from "@chakra-ui/react";
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from "react-icons/ri";
 
 export function Header() {
   return (
@@ -33,6 +33,35 @@ export function Header() {
         />
 
         <Icon as={RiSearchLine} fontSize='20' />
+      </Flex>
+
+      <Flex align='center' ml='auto'>
+        <HStack
+          spacing='8'
+          mx='8'
+          pr='8'
+          py='1'
+          color='gray.300'
+          borderRightWidth={1}
+          borderColor='gray.700'>
+          <Icon as={RiNotificationLine} fontSize='20' />
+          <Icon as={RiUserAddLine} fontSize='20' />
+        </HStack>
+
+        <Flex align='center'>
+          <Box mr='4' textAlign='right'>
+            <Text>Thiago Morato</Text>
+            <Text color='gray.300' fontSize='small'>
+              thiagosmorato@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar
+            size='md'
+            name='Thiago Morato'
+            src='https://avatars.githubusercontent.com/u/96215280?s=400&u=7f4080f81a70cd988c88f005cbd58976cabcbcf0&v=4'
+          />
+        </Flex>
       </Flex>
     </Flex>
   );
