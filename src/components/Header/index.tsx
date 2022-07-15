c36b0b6dc6027d245a9efa6225d9abac0b016f9b
ThiagoSilvaMorato <thiago.morato@dartdigital.com.br>
2022-07-15 14:52:58 -0300

    Header responsivo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,25 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, useBreakpointValue } from "@chakra-ui/react";
 import { Profile } from "./Profile";
 import { NotificationsNav } from "./NotificationsNav";
 import { Logo } from "./Logo";
 import { SearchBox } from "./SearchBox";
 
 export function Header() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Flex as='header' w='100%' maxWidth={1480} h='20' mx='auto' mt='4' px='6' align='center'>
       <Logo />
 
-      <SearchBox />
+      {isWideVersion && <SearchBox />}
 
       <Flex align='center' ml='auto'>
         <NotificationsNav />
 
-        <Profile />
+        <Profile showProfileData={isWideVersion} />
       </Flex>
     </Flex>
   );
