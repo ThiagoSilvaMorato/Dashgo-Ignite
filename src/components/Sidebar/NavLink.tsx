c0f69c114ca4d5cf4250dc6038351d754e03ec77
ThiagoSilvaMorato <thiago.morato@dartdigital.com.br>
2022-07-15 15:35:27 -0300

    Fluxo de navegação

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,20 +1,22 @@
-import { Icon, Link, Text, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
-import { resolvePtr } from "dns/promises";
+import { Icon, Link as ChakraLink, Text, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
 import { ElementType } from "react";
-import { RiDashboardLine } from "react-icons/ri";
+import Link from "next/link";
 
 interface NavLinkProps extends ChakraLinkProps {
   icon: ElementType;
   children: string;
+  href: string;
 }
 
-export function NavLink({ icon, children, ...rest }: NavLinkProps) {
+export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
   return (
-    <Link display='flex' {...rest}>
-      <Icon as={icon} fontSize='20' />
-      <Text ml='4' fontWeight='medium'>
-        {children}
-      </Text>
+    <Link href={href} passHref>
+      <ChakraLink display='flex' {...rest}>
+        <Icon as={icon} fontSize='20' />
+        <Text ml='4' fontWeight='medium'>
+          {children}
+        </Text>
+      </ChakraLink>
     </Link>
   );
 }
